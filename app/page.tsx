526082202b0ab82af9ba70663c3b62ebb0fedfb1
Violani <violanijulia@gmail.com>
2025-08-29 14:05:34 +1000

    change hello

--- app/page.tsx
+++ app/page.tsx
@@ -20,9 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li className="tracking-[-.01em]">
-            hiiiii slay.
-          </li>
+          <li className="tracking-[-.01em]">hello</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
