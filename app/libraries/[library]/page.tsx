56ef197cf54157530921e76331f8ac10483a2330
nhnong <nhi.nong@student.unimelb.edu.au>
2025-08-30 00:10:15 +1000

    link works

---
+++ app/libraries/[library]/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import { libraries } from "@/app/data/libraries";
+import { notFound } from "next/navigation";
+import Link from "next/link";
+
+interface LibraryPageProps {
+  params: { library: string }; 
+}
+
+export default function LibraryPage({ params }: LibraryPageProps) {
+    const library = libraries.find((lib: typeof libraries[number]) => lib.urlname === params.library);
+    if (!library) {
+        return notFound();
+    }
+    return (
+        <main className="p-4">
+            <h1 className="text-2xl font-bold mb-4">{library.name}</h1>
+            <p>seats: {library.seat.join(", ")}</p>
+            <p>noise: {library.noise.join(", ")}</p>
+            <p>charging: {library.charging}</p>
+        </main>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
  
   return (
     <main>
-      <div className="text-center">
-        <h1 className="text-center font-bold text-[48px] leading-none">
+      <div className="text-center mt-8 mb-8">
+        <h1 className="text-center font-bold text-[30px] leading-none">
           Welcome, User!
         </h1>
         <p>Where would you like to study today?</p>
@@ -15,16 +15,18 @@ export default function Home() {
       <div className="text-center flex flex-col sm:flex-row justify-center items-center gap-5">
         {libraries.map((lib) => (
           <div key={lib.name}>
-            <h2>{lib.name}</h2>
-            <p>seats: {lib.seat.join(", ")}</p>
-            <p>noise: {lib.noise.join(", ")}</p>
-            <p>charging: {lib.charging}</p>
             <div className="relative h-55 w-80">
               <Image              
                 src={`/${lib.imgsrc}`}
                 alt={lib.name}
                 fill
+                className="object-cover rounded-lg"
               />
+              <div className="absolute bottom-0 w-full bg-[#0E2647] bg-opacity-60 text-white text-center py-2 rounded-b-lg">
+                <Link href={`/libraries/${lib.urlname}`} className="hover:underline">
+                  {lib.name}
+                </Link>
+              </div>
             </div>
           </div>
         ))}
@@ -33,5 +35,9 @@ export default function Home() {
   );
 }
 
-
+// library features, will add on another page 
+//             <h2>{lib.name}</h2>
+//            <p>seats: {lib.seat.join(", ")}</p>
+//            <p>noise: {lib.noise.join(", ")}</p>
+//            <p>charging: {lib.charging}</p>
 
