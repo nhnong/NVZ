66f119acebbed191419bfab836b190c9c541ab1d
nhnong <nhi.nong@student.unimelb.edu.au>
2025-08-29 23:04:57 +1000

    has images

--- app/page.tsx
+++ app/page.tsx
@@ -12,13 +12,20 @@ export default function Home() {
         </h1>
         <p>Where would you like to study today?</p>
       </div>
-      <div className="text-center">
+      <div className="text-center flex flex-col sm:flex-row justify-center items-center gap-5">
         {libraries.map((lib) => (
           <div key={lib.name}>
             <h2>{lib.name}</h2>
             <p>seats: {lib.seat.join(", ")}</p>
             <p>noise: {lib.noise.join(", ")}</p>
             <p>charging: {lib.charging}</p>
+            <div className="relative h-55 w-80">
+              <Image              
+                src={`/${lib.imgsrc}`}
+                alt={lib.name}
+                fill
+              />
+            </div>
           </div>
         ))}
       </div>
