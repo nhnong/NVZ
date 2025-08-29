d88153277add78bf2c83435c3e259df14ade084e
Violani <violanijulia@gmail.com>
2025-08-29 18:05:14 +1000

    trying h1 to be in the center

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,19 @@ const allnoise = new Set(["Quiet", "Group Study"]);
 const allseat = new Set(["Sofa", "Single Desk", "Group Table"]);
 const allcharging = new Set(["Charging", "No Charging"]);
 
-const library1 = createLibrary("name", ["Quiet"], ["Sofa", "Single Desk"], "Charging");
+const library1 = createLibrary(
+  "name",
+  ["Quiet"],
+  ["Sofa", "Single Desk"],
+  "Charging"
+);
 
-function createLibrary(name: string, noise: string[], seat: string[], charging: string){
-
+function createLibrary(
+  name: string,
+  noise: string[],
+  seat: string[],
+  charging: string
+) {
   if (!name) {
     throw new Error("Name is required");
   }
@@ -17,19 +26,18 @@ function createLibrary(name: string, noise: string[], seat: string[], charging: 
 
   const library = {
     name: name,
-    noise: noise.filter(n => noise.includes(n)),
-    seat: seat.filter(s => seat.includes(s)),
-    charging: charging
-  }
+    noise: noise.filter((n) => noise.includes(n)),
+    seat: seat.filter((s) => seat.includes(s)),
+    charging: charging,
+  };
 
   return library;
 }
 
 export default function Home() {
-  
   return (
     <main>
-      <h1>Welcome, User!</h1>
+      <h1 className="text-3x1 font-bold text-center">Welcome, User!</h1>
       <p>Where would you like to study today?</p>
       <div>
         <p>name: {library1.name}</p>
