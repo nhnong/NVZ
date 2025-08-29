630e14e28c037a9b68bf400ae50a68cf3f5ec9f7
nhnong <nhi.nong@student.unimelb.edu.au>
2025-08-29 22:41:44 +1000

    loop thru libs

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,12 @@ export default function Home() {
  
   return (
     <main>
-      <h1 className="text-center font-bold text-[48px] leading-none">
-        Welcome, User!
-      </h1>
-      <p>Where would you like to study today?</p>
+      <div className="text-center">
+        <h1 className="text-center font-bold text-[48px] leading-none">
+          Welcome, User!
+        </h1>
+        <p>Where would you like to study today?</p>
+      </div>
       <div className="text-center">
         {libraries.map((lib) => (
           <div key={lib.name}>
