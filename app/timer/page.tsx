51dd45c6d4b0b055a645d5bf4ebcff78fa2fa297
Anh Trinh <namanht@student.unimelb.edu.au>
2025-08-30 16:36:03 +1000

    pomodoro timer

--- app/timer/page.tsx
+++ app/timer/page.tsx
@@ -1,12 +1,12 @@
-import Timer from "@/public/TimerComponents/timer"
+import Timer from "@/pomodoro-timer/src/app/_components/timer/timer"
 export default function TimerPage(){
     return (
-        <div className="bg-gray-50">
-            <div className="bg-gray-50">
-                
-            <Timer/>
-            </div>
-            </div>
-        
+        <main
+            className="min-h-screen flex flex-col items-center justify-center p-6 bg-cover bg-center"
+            style={{ backgroundImage: "url('/pomodoro-screen.png')" }}>
+        <div className="bg-white/80 backdrop-blur-md p-6 rounded-2xl shadow-lg w-full max-w-md">
+            <Timer />
+        </div>
+        </main>
     )
 }
