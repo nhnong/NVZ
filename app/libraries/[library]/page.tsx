ebed132440df96d08a0f6683baf641793dfbba48
nhnong <nhi.nong@student.unimelb.edu.au>
2025-08-30 13:20:39 +1000

    static toggle buttons on library page

--- app/libraries/[library]/page.tsx
+++ app/libraries/[library]/page.tsx
@@ -8,11 +8,9 @@ interface LibraryPageProps {
   params: { library: string };
 }
 
-
 export default async function LibraryPage({ params }: any) {
     const resolvedParams = await params;
     const libraryUrl = resolvedParams.library;
-
     const library = libraries.find((lib) => lib.urlname === libraryUrl);
     if (!library) {
         return notFound();
@@ -35,7 +33,7 @@ export default async function LibraryPage({ params }: any) {
             ))}</p>
             <p className="text-[12px] font-bold mb-6"><span>🌙</span>After-hours study zone available</p>
 
-            <div className="space-y-4">
+            <div className="space-y-4 mb-10">
 
                 <div className="flex items-center gap-4">
                     <span className="w-20 font-semibold text-[12px]">Seats:</span> {/* fixed width */}
@@ -57,6 +55,94 @@ export default async function LibraryPage({ params }: any) {
                 </div>
             </div>
 
+           
+            <div className="space-y-4 mb-3">
+                <input type="checkbox" id="seat-toggle" className="hidden peer" />
+                <label
+                    htmlFor="seat-toggle"
+                    className="block w-full px-5 py-4 bg-[#D9E7FC] hover:bg-[#9eb9e0] text-[12px] font-bold shadow rounded-lg cursor-pointer "
+                >
+                    <div className="flex justify-between">
+                        <div className="flex flex-shrink-0">
+                            <Image src="/sofa_available.png" alt="Seat Icon" width={20} height={20} className="mr-2" />
+                            <p>Sofa</p>
+                        </div>
+                        <p>Available: #</p>
+                    </div>
+                </label>
+                <div className="peer-checked:block hidden pl-4 mt-2 space-y-2">
+                    <div className="flex justify-between px-5 py-2.5 rounded-lg bg-[#D9D9D9] text-[12px]">
+                        <p>Level 1</p>
+                        <p>Available: #</p>
+                    </div>
+                    <div className="flex justify-between px-5 py-2.5 rounded-lg bg-[#D9D9D9] text-[12px]">
+                        <p>Level 2</p>
+                        <p>Available: #</p>
+                    </div>
+                    <div className=" flex justify-between px-5 py-2.5 rounded-lg bg-[#D9D9D9] text-[12px]">
+                        <p>Level 3</p>
+                        <p>Available: #</p>
+                    </div>
+                </div>
+            </div>
+            <div className="space-y-4 mb-3">
+                <input type="checkbox" id="desk-toggle" className="hidden peer" />
+                <label
+                    htmlFor="desk-toggle"
+                    className="block w-full px-5 py-4 bg-[#D9E7FC] hover:bg-[#9eb9e0] text-[12px] font-bold shadow rounded-lg cursor-pointer "
+                >
+                    <div className="flex justify-between">
+                        <div className="flex flex-shrink-0">
+                            <Image src="/desk.png" alt="Desk Icon" width={20} height={20} className="mr-2" />
+                            <p>Single Desk</p>
+                        </div>
+                        <p>Available: #</p>
+                    </div>
+                </label>
+                <div className="peer-checked:block hidden pl-4 mt-2 space-y-2">
+                    <div className="flex justify-between px-5 py-2.5 rounded-lg bg-[#D9D9D9] text-[12px]">
+                        <p>Level 1</p>
+                        <p>Available: #</p>
+                    </div>
+                    <div className="flex justify-between px-5 py-2.5 rounded-lg bg-[#D9D9D9] text-[12px]">
+                        <p>Level 2</p>
+                        <p>Available: #</p>
+                    </div>
+                    <div className=" flex justify-between px-5 py-2.5 rounded-lg bg-[#D9D9D9] text-[12px]">
+                        <p>Level 3</p>
+                        <p>Available: #</p>
+                    </div>
+                </div>
+            </div>
+            <div className="space-y-4 mb-3">
+                <input type="checkbox" id="group-toggle" className="hidden peer" />
+                <label
+                    htmlFor="group-toggle"
+                    className="block w-full px-5 py-4 bg-[#D9E7FC] hover:bg-[#9eb9e0] text-[12px] font-bold shadow rounded-lg cursor-pointer "
+                >
+                    <div className="flex justify-between">
+                        <div className="flex flex-shrink-0">
+                            <Image src="/group.png" alt="Group Icon" width={20} height={20} className="mr-2" />
+                            <p>Group Table</p>
+                        </div>
+                        <p>Available: #</p>
+                    </div>
+                </label>
+                <div className="peer-checked:block hidden pl-4 mt-2 space-y-2">
+                    <div className="flex justify-between px-5 py-2.5 rounded-lg bg-[#D9D9D9] text-[12px]">
+                        <p>Level 1</p>
+                        <p>Available: #</p>
+                    </div>
+                    <div className="flex justify-between px-5 py-2.5 rounded-lg bg-[#D9D9D9] text-[12px]">
+                        <p>Level 2</p>
+                        <p>Available: #</p>
+                    </div>
+                    <div className="flex justify-between px-5 py-2.5 rounded-lg bg-[#D9D9D9] text-[12px]">
+                        <p>Level 3</p>
+                        <p>Available: #</p>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
